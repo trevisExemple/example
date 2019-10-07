5629b95648ac5c1436c468baf51308556bd870c5
littlehustla89 <ipodnew2013@yandex.ru>
2019-10-07 05:31:47 +0300

    first commit

---
+++ src/main/java/example/govno3/Govno3Application.java
@@ -0,0 +1,12 @@
+package example.govno3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Govno3Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Govno3Application.class, args);
+    }
+}
---
+++ src/main/java/example/govno3/controller/MyController.java
@@ -0,0 +1,24 @@
+package example.govno3.controller;
+
+import example.govno3.model.Articles;
+import example.govno3.service.SiteParsingService;
+import example.govno3.utils.AppProperty;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyController {
+
+    @Autowired
+    protected SiteParsingService siteParsingService;
+
+    @RequestMapping("/")
+    public String handleRequest(Model model) {
+        Articles articles = siteParsingService.readJsonFile(
+                AppProperty.getProperty("pathToSave") + AppProperty.getProperty("fileName"));
+        model.addAttribute("news", articles.getArticles());
+        return "news";
+    }
+}
